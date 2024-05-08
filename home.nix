9cc873e884f052e9eb40929efbfffe2f48f507a8
VageLO <48802151+VageLO@users.noreply.github.com>
2024-05-08 10:15:20 +0300

    Update home.nix

--- home.nix
+++ home.nix
@@ -35,34 +35,32 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+    ".config/nvim" = {
+      recursive = true;
+      source = ../dotfiles/nvim;
+    };
+    ".config/alacritty" = {
+      recursive = true;
+      source = ../dotfiles/alacritty;
+    };
+    ".config/i3" = {
+      recursive = true;
+      source = ../dotfiles/i3;
+    };
+    ".config/i3status" = {
+      recursive = true;
+      source = ../dotfiles/i3status;
+    };
+    ".config/lazygit" = {
+      recursive = true;
+      source = ../dotfiles/lazygit;
+    };
+    ".config/tmux" = {
+      recursive = true;
+      source = ../dotfiles/tmux;
+    };
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/vagelo/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
     EDITOR = "nvim";
   };
