63cb8fef6fb410b88b166214d886ebd4d4483d7e
VageLO <48802151+VageLO@users.noreply.github.com>
2024-09-12 13:29:27 +0300

    update: mpv

--- modules/mpv.nix
+++ modules/mpv.nix
@@ -4,11 +4,8 @@
     programs.mpv.enable = true;
     home.file = {
         ".config/mpv" = {
-            source = pkgs.fetchgit {
-                url = "https://github.com/VageLO/mpv2anki";
-                hash = "sha256-tTss8yN18uGvoy1jWOfHCHh79sAv9MOlbq66YMd9aCA=";
-            };
             recursive = true;
+            source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/home-manager/dotfiles/mpv";
         };
         ".local/share/Anki2/mpv.conf" = {
             text = ''
@@ -19,19 +16,16 @@
         };
     };
 
-    #home.file = {
-    #    ".config/mpv" = {
-    #        recursive = true;
-    #        source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/app/mpv2anki";
-    #    };
-    #};
-
     home.packages = with pkgs; [
+        chromedriver
+        chromium
         ffmpeg_7
         (python3.withPackages (ps: with ps; [
             pip
             requests
             beautifulsoup4
+            selenium
+            webdriver-manager
         ]))
     ];
 }
