83cf5f9c171f2ba91c54c5790b0b6c746726fb5b
VageLO <48802151+VageLO@users.noreply.github.com>
2024-11-02 18:41:03 +0300

    update: i18n

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -34,15 +34,15 @@
   i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "et_EE.UTF-8";
-    LC_IDENTIFICATION = "et_EE.UTF-8";
-    LC_MEASUREMENT = "et_EE.UTF-8";
-    LC_MONETARY = "et_EE.UTF-8";
-    LC_NAME = "et_EE.UTF-8";
-    LC_NUMERIC = "et_EE.UTF-8";
-    LC_PAPER = "et_EE.UTF-8";
-    LC_TELEPHONE = "et_EE.UTF-8";
-    LC_TIME = "et_EE.UTF-8";
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
   };
 
   # Configure i3wm
