90b0ec525b57e410a22936e490068e0a9a830f6b
VageLO <48802151+VageLO@users.noreply.github.com>
2025-04-22 21:21:21 +0300

    update: nix flake

--- modules/nvim/harpoon.nix
+++ modules/nvim/harpoon.nix
@@ -1,15 +1,16 @@
 {
-    programs.nixvim.plugins.harpoon = {
-        enable = true;
-        keymaps = {
-            addFile = "<leader>hh";
-            toggleQuickMenu = "<leader>h";
-            navFile = {
-                "1" = "<leader>1";
-                "2" = "<leader>2";
-                "3" = "<leader>3";
-                "4" = "<leader>4";
-            };
-        };
+  programs.nixvim = {
+    plugins.harpoon = {
+      enable = true;
     };
+    keymaps = [
+      { mode = "n"; key = "<leader>hh"; action.__raw = "function() require'harpoon':list():add() end"; }
+      { mode = "n"; key = "<leader>h"; action.__raw = "function() require'harpoon'.ui:toggle_quick_menu(require'harpoon':list()) end"; }
+      { mode = "n"; key = "<leader>1"; action.__raw = "function() require'harpoon':list():select(1) end"; }
+      { mode = "n"; key = "<leader>2"; action.__raw = "function() require'harpoon':list():select(2) end"; }
+      { mode = "n"; key = "<leader>3"; action.__raw = "function() require'harpoon':list():select(3) end"; }
+      { mode = "n"; key = "<leader>4"; action.__raw = "function() require'harpoon':list():select(4) end"; }
+      { mode = "n"; key = "<leader>5"; action.__raw = "function() require'harpoon':list():select(5) end"; }
+    ];
+  };
 }
