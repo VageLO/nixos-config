5d782cb36a63cf0cb86ccdf7575ddef21771ebaa
VageLO <48802151+VageLO@users.noreply.github.com>
2025-09-22 23:14:25 +0300

    update: starship

--- modules/starship.nix
+++ modules/starship.nix
@@ -4,6 +4,7 @@
     enable = true;
     enableZshIntegration = true;
     settings = {
+      add_newline = false;
       battery = {
         full_symbol = "🔋 ";
         charging_symbol = "⚡️ ";
@@ -21,6 +22,29 @@
         impure_msg = "[impure shell](bold red)";
         pure_msg = "[pure shell](bold green)";
         format = "via [☃️ $state( ($name))](bold blue) ";
+      };
+      git_branch = {
+        symbol = "🌱 ";
+      };
+      git_commit = {
+        commit_hash_length = 4;
+        tag_symbol = "🔖 ";
+      };
+      git_state = {
+        format = "[($state($progress_current of $progress_total))]($style) ";
+        cherry_pick = "[🍒 PICKING](bold red)";
+      };
+      git_status = {
+        conflicted = "🏳";
+        ahead = "🏎💨";
+        behind = "😰";
+        diverged = "😵";
+        untracked = "🤷‍";
+        stashed = "📦";
+        modified = "📝";
+        staged = "[++($count)](green)";
+        renamed = "👅";
+        deleted = "🗑";
       };
     };
   };
